e03b2bb0b88b8821ad2adacf2f661309d053a848
Oleksii Chyharov <chyharov@gmail.com>
2022-10-02 22:09:54 +0300

    feat: express

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { connect } = require("mongoose");
+const express = require("express");
 
 const { mongoURL } = require("./config");
 
-connect(mongoURL, { dbName: "database" })
-  .then(() => console.log("MangoDB connected"))
-  .catch((err) => console.log(err.message));
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Main Page</h1>");
+});
+
+app.listen(5001, () => {
+  console.log("Server started");
+
+  connect(mongoURL, { dbName: "database" })
+    .then(() => console.log("MangoDB connected"))
+    .catch((err) => console.log(err.message));
+});
